fd9828bcfec1562634b49aef824bb70cff221123
sjfarinas <sjfarinas@gmail.com>
2021-07-10 10:50:01 -0600

    adding flask funcionality to store data

--- main.py
+++ main.py
@@ -1,21 +1,114 @@
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from dotenv import dotenv_values
+import datetime
 
-response = requests.get("https://api.npoint.io/29553edabcf2b6d4a0b7")
-data = response.json()
+config = dotenv_values(".env")
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = config.get('SECRET_KEY')
+ckeditor = CKEditor(app)
+Bootstrap(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
 
 
 @app.route('/')
-def home():
-    return render_template("index.html", posts=data)
+def get_all_posts():
+    posts = BlogPost.query.all()
+    return render_template("index.html", all_posts=posts)
 
 
-@app.route('/blog/<num>')
-def blog(num):
-    return render_template("post.html", post=data[int(num)-1])
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = BlogPost.query.get(index)
+    return render_template("post.html", post=requested_post)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/new-post', methods=["GET", "POST"])
+def new_post():
+    form = CreatePostForm()
+
+    if form.validate_on_submit():
+        post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            date=datetime.datetime.now().strftime("%B %d, %Y"),
+            body=form.body.data,
+            author=form.author.data,
+            img_url=form.img_url.data
+        )
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('get_all_posts'))
+
+    return render_template("make-post.html", form=form)
+
+
+@app.route('/edit-post/<post_id>', methods=["GET", "POST"])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.subtitle = form.subtitle.data
+        post.img_url = form.img_url.data
+        post.author = form.author.data
+        post.body = form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", index=post.id))
+    return render_template("make-post.html", form=form, is_edit=True)
+
+
+@app.route('/delete/<post_id>')
+def delete_post(post_id):
+    post = BlogPost.query.get(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
